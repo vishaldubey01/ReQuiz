adc52af2eb7a1d506431474b3d39cbe9057038af
Ansh Nanda <anshnanda10@gmail.com>
2020-12-06 12:14:24 +0530

    Add homepage style and footer component

--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
+import Footer from "../components/footer";
 import styles from "../styles/Home.module.css";
 
 export default function Dashboard() {
@@ -554,16 +555,7 @@ export default function Dashboard() {
           </div>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Footer from "../components/footer";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
@@ -7,7 +9,10 @@ export default function Home() {
         <title>ReQuiz</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      hi
+      <main>
+        <Hero />
+      </main>
+      <Footer />
     </div>
   );
 }
