95f0797b97618e16d4a7a40a91071000a3fe9af1
Ansh Nanda <anshnanda10@gmail.com>
2020-12-06 13:05:12 +0530

    Add navbar functionality

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -3,14 +3,16 @@ import styles from "../styles/Home.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{" "}
-        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-      </a>
+      <div className="inline">
+        Developed with ❤️ at
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Duke_Athletics_logo.svg/150px-Duke_Athletics_logo.svg.png"
+          alt="Duke Logo"
+          className={styles.logo}
+          style={{ display: "inline" }}
+        />{" "}
+        by Vishal, Rami, and Ansh
+      </div>
     </footer>
   );
 }
