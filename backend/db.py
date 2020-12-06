832de840f1540ca110aae4940d07bc9e88aac40c
Rami Sbahi <sbahirami@gmail.com>
2020-12-06 10:39:41 -0500

    cloud, database, endpoints

--- backend/db.py
+++ backend/db.py
@@ -201,8 +201,8 @@ def add_text_row(text, userid):
     authtoken = get_auth_token()
 
     url = "https://"+ASTRA_CLUSTER_ID+"-"+ASTRA_CLUSTER_REGION+".apps.astra.datastax.com/api/graphql/fucknigel"
-    
-    
+
+
     headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/json',
@@ -212,8 +212,9 @@ def add_text_row(text, userid):
     'Origin': 'https://'+ASTRA_CLUSTER_ID+'-'+ASTRA_CLUSTER_REGION+'.apps.astra.datastax.com',
     'x-cassandra-token': authtoken
     }
+    text = "hi" # why do we need to store text?
     response = requests.request("POST", url, headers=headers, json={"query":"mutation {\n  bruh: inserttext(\n    value: { id: \""+id+"\", text: \""+text+"\", userid: \""+text+"\" }\n    options: { consistency: LOCAL_QUORUM }\n  ) {\n    value {\n      id\n    }\n  }\n}\n"})
-    
+
 
     print(response.json())
     retid = response.json()['data']['bruh']['value']['id']
@@ -225,8 +226,8 @@ def add_question_row(question_content, answer, qtype, textid):
     authtoken = get_auth_token()
 
     url = "https://"+ASTRA_CLUSTER_ID+"-"+ASTRA_CLUSTER_REGION+".apps.astra.datastax.com/api/graphql/fucknigel"
-    
-    
+
+
     headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/json',
@@ -237,7 +238,7 @@ def add_question_row(question_content, answer, qtype, textid):
     'x-cassandra-token': authtoken
     }
     response = requests.request("POST", url, headers=headers, json={"query":"mutation {\n  bruh: insertquestions(\n    value: { id: \""+id+"\", question: \""+question_content+"\", answer: \""+answer+"\", rating: \""+str(1.00)+"\", textid: \""+textid+"\", type: \""+qtype+"\" }\n    options: { consistency: LOCAL_QUORUM }\n  ) {\n    value {\n      id\n    }\n  }\n}\n"})
-    
+
     print(response.json())
     return response.json()
 
@@ -245,8 +246,8 @@ def get_all_text_questions(textid):
     authtoken = get_auth_token()
 
     url = "https://"+ASTRA_CLUSTER_ID+"-"+ASTRA_CLUSTER_REGION+".apps.astra.datastax.com/api/graphql/fucknigel"
-    
-    
+
+
     headers = {
     'Accept-Encoding': 'gzip, deflate, br',
     'Content-Type': 'application/json',
@@ -259,7 +260,7 @@ def get_all_text_questions(textid):
     responseQuestions = requests.request("POST", url, headers=headers, json={"query":"query {\n  questions(options:{limit: 10000}) {\n    values {\n      id\n      question\n      answer\n    textid\n    type\n    }\n  }\n}"})
     responseTexts = requests.request("POST", url, headers=headers, json={"query":"query {\n  text(options:{limit: 10000}) {\n    values {\n      id\n      text\n      userid\n    }\n  }\n}"})
 
-    
+
     questionRes = responseQuestions.json()
     textRes = responseTexts.json()
 
@@ -269,11 +270,11 @@ def get_all_text_questions(textid):
     for question in questionRes['data']['questions']['values']:
         if question['textid'] == textid:
             questionsMatch.append(question)
-    
+
     for text in textRes['data']['text']['values']:
         if text['id'] == textid:
             textMatch = text
-    
+
     ret = {
         "questions": questionsMatch,
         "textInformation": textMatch
@@ -284,7 +285,9 @@ def get_all_text_questions(textid):
 
 
 # THESE ARE TESTING RUNS:
-
+get_all_text_questions("7aa65814-37d5-11eb-9df7-acde48001122")
 #get_all_text_questions("d6354fb8-37bd-11eb-8ef2-acde48001122")
-#text_id_returned = add_text_row("hello aslkdjfalksdjfklasd vishal", "1323423232")
-#add_question_row("What is a samp hi hello bye question?", "an example dmb query", "free response", text_id_returned)
+#get_all_text_questions("91fc7528-37c8-11eb-a809-acde48001122")
+#get_all_text_questions("bf679b14-37c8-11eb-a614-acde48001122")
+#text_id_returned = add_text_row("I'm a cool guy", "69")
+#add_question_row("What is a sam?", "an example dmb query", "free response", text_id_returned)
